4c9988238fe4739a7ad9a306b02d37fd99c1c2c8
jlang_admin <jlang_admin@evelix-w06.evelix.local>
2024-09-25 10:21:07 +0200

    v.2.3.0.1054

--- src/lib/Config.ts
+++ src/lib/Config.ts
@@ -1,86 +1,129 @@
-require('dotenv').config();
+require("dotenv").config();
 
 export interface Config {
-    env?: string;
-    services: string[];
-    secret: string;
-    accessToken: string;
+  env?: string;
+  services: string[];
+  secret: string;
+  accessToken: string;
+  port: number;
+  manifestServerUrl?: string;
+  manifestSearchUrl?: string;
+  collectionSearchUrl?: string;
+  imageServerUrl?: string;
+  logLevel: string;
+  baseUrl: string;
+  loginDisabled: boolean;
+  solr: null | {
+    host: string;
     port: number;
-    manifestSearchUrl?: string;
-    imageServerUrl?: string;
-    logLevel: string;
-    baseUrl: string;
-    loginDisabled: boolean;
-    solr: null | {
-        host: string;
-        port: number;
-        delay: number;
-        core: string;
-    };
+    delay: number;
+    core: string;
+    maxRows: number;
+  };
 }
 
 const config: Config = {
-    env: process.env.NODE_ENV,
-    imageServerUrl: process.env.IIIF_SERVER_IMAGE_SERVER_URL,
-    manifestSearchUrl: process.env.IIIF_SERVER_MANIFEST_SEARCH_URL,
+  env: process.env.NODE_ENV,
+  imageServerUrl: process.env.IIIF_SERVER_IMAGE_SERVER_URL,
+  manifestServerUrl: process.env.IIIF_SERVER_MANIFEST_SERVER_URL,
+  manifestSearchUrl: process.env.IIIF_SERVER_MANIFEST_SEARCH_URL,
+  collectionSearchUrl: process.env.IIIF_SERVER_COLLECTION_SEARCH_URL,
 
-    services: (() => {
-        if (!process.env.IIIF_SERVER_SERVICES || (process.env.IIIF_SERVER_SERVICES === 'null'))
-            throw new Error('Services to run are not defined');
-        return process.env.IIIF_SERVER_SERVICES.split(',');
-    })(),
+  services: (() => {
+    if (
+      !process.env.IIIF_SERVER_SERVICES ||
+      process.env.IIIF_SERVER_SERVICES === "null"
+    )
+      throw new Error("Services to run are not defined");
+    return process.env.IIIF_SERVER_SERVICES.split(",");
+  })(),
 
-    secret: (() => {
-        if (!process.env.IIIF_SERVER_SECRET || (process.env.IIIF_SERVER_SECRET === 'null'))
-            throw new Error('Secret is not defined');
-        return process.env.IIIF_SERVER_SECRET;
-    })(),
+  secret: (() => {
+    if (
+      !process.env.IIIF_SERVER_SECRET ||
+      process.env.IIIF_SERVER_SECRET === "null"
+    )
+      throw new Error("Secret is not defined");
+    return process.env.IIIF_SERVER_SECRET;
+  })(),
 
-    accessToken: (() => {
-        if (!process.env.IIIF_SERVER_ACCESS_TOKEN || (process.env.IIIF_SERVER_ACCESS_TOKEN === 'null'))
-            throw new Error('The access token is not defined');
-        return process.env.IIIF_SERVER_ACCESS_TOKEN;
-    })(),
+  accessToken: (() => {
+    if (
+      !process.env.IIIF_SERVER_ACCESS_TOKEN ||
+      process.env.IIIF_SERVER_ACCESS_TOKEN === "null"
+    )
+      throw new Error("The access token is not defined");
+    return process.env.IIIF_SERVER_ACCESS_TOKEN;
+  })(),
 
-    port: (() => {
-        const port = process.env.IIIF_SERVER_PORT ? parseInt(process.env.IIIF_SERVER_PORT) : 0;
-        return (port >= 0) ? port : 3333;
-    })(),
+  port: (() => {
+    const port = process.env.IIIF_SERVER_PORT
+      ? parseInt(process.env.IIIF_SERVER_PORT)
+      : 0;
+    return port >= 0 ? port : 3333;
+  })(),
 
-    logLevel: (() => {
-        return (process.env.IIIF_SERVER_LOG_LEVEL && (process.env.IIIF_SERVER_LOG_LEVEL !== 'null'))
-            ? process.env.IIIF_SERVER_LOG_LEVEL : 'debug';
-    })(),
+  logLevel: (() => {
+    return process.env.IIIF_SERVER_LOG_LEVEL &&
+      process.env.IIIF_SERVER_LOG_LEVEL !== "null"
+      ? process.env.IIIF_SERVER_LOG_LEVEL
+      : "debug";
+  })(),
 
-    baseUrl: (() => {
-        if (!process.env.IIIF_SERVER_BASE_URL || (process.env.IIIF_SERVER_BASE_URL === 'null'))
-            throw new Error('The base url is not defined');
-        return process.env.IIIF_SERVER_BASE_URL;
-    })(),
+  baseUrl: (() => {
+    if (
+      !process.env.IIIF_SERVER_BASE_URL ||
+      process.env.IIIF_SERVER_BASE_URL === "null"
+    )
+      throw new Error("The base url is not defined");
+    return process.env.IIIF_SERVER_BASE_URL;
+  })(),
 
-    loginDisabled: (() => {
-        const loginDisabled = process.env.IIIF_SERVER_LOGIN_DISABLED;
-        return ((loginDisabled !== undefined) && (loginDisabled.toLowerCase() === 'true' || loginDisabled === '1'));
-    })(),
+  loginDisabled: (() => {
+    const loginDisabled = process.env.IIIF_SERVER_LOGIN_DISABLED;
+    return (
+      loginDisabled !== undefined &&
+      (loginDisabled.toLowerCase() === "true" || loginDisabled === "1")
+    );
+  })(),
 
-    solr: (() => {
-        const host = (process.env.IIIF_SERVER_SOLR_HOST && (process.env.IIIF_SERVER_SOLR_HOST !== 'null'))
-            ? process.env.IIIF_SERVER_SOLR_HOST : 'localhost';
-        const port = process.env.IIIF_SERVER_SOLR_PORT && parseInt(process.env.IIIF_SERVER_SOLR_PORT) > 0
-            ? parseInt(process.env.IIIF_SERVER_SOLR_PORT) : 8983;
-        const delay = process.env.IIIF_SERVER_SOLR_DELAY && parseInt(process.env.IIIF_SERVER_SOLR_DELAY) > 0
-            ? parseInt(process.env.IIIF_SERVER_SOLR_DELAY) : 100;
-        const core = (process.env.IIIF_SERVER_SOLR_CORE && (process.env.IIIF_SERVER_SOLR_CORE !== 'null'))
-            ? process.env.IIIF_SERVER_SOLR_CORE : 'iiif';
+  solr: (() => {
+    const host =
+      process.env.IIIF_SERVER_SOLR_HOST &&
+      process.env.IIIF_SERVER_SOLR_HOST !== "null"
+        ? process.env.IIIF_SERVER_SOLR_HOST
+        : "localhost";
+    const port =
+      process.env.IIIF_SERVER_SOLR_PORT &&
+      parseInt(process.env.IIIF_SERVER_SOLR_PORT) > 0
+        ? parseInt(process.env.IIIF_SERVER_SOLR_PORT)
+        : 8983;
+    const delay =
+      process.env.IIIF_SERVER_SOLR_DELAY &&
+      parseInt(process.env.IIIF_SERVER_SOLR_DELAY) > 0
+        ? parseInt(process.env.IIIF_SERVER_SOLR_DELAY)
+        : 100;
+    const core =
+      process.env.IIIF_SERVER_SOLR_CORE &&
+      process.env.IIIF_SERVER_SOLR_CORE !== "null"
+        ? process.env.IIIF_SERVER_SOLR_CORE
+        : "iiif";
+    const maxRows =
+      process.env.IIIF_SERVER_SOLR_MAX_ROWS &&
+      parseInt(process.env.IIIF_SERVER_SOLR_MAX_ROWS) > 0
+        ? parseInt(process.env.IIIF_SERVER_SOLR_MAX_ROWS)
+        : 100;
 
-        return {host, port, delay, core};
-    })()
+    return { host, port, delay, core, maxRows };
+  })(),
 };
 
 // For test purposes
-export function setConfig<P extends keyof Config, V extends Config[P]>(property: P, value: V): void {
-    if (config.env === 'test')
-        config[property] = value;
+export function setConfig<P extends keyof Config, V extends Config[P]>(
+  property: P,
+  value: V
+): void {
+  if (config.env === "test") config[property] = value;
 }
 
 export default config;
--- src/search/router.ts
+++ src/search/router.ts
@@ -1,166 +1,364 @@
-import {Context} from 'koa';
-import * as Router from 'koa-router';
-
-import HttpError from '../lib/HttpError';
-import logger from '../lib/Logger';
-import config from '../lib/Config';
-import uuid = require("uuid");
-const axios = require('axios');
-import * as _ from "lodash";
+import { Context } from "koa";
+import * as Router from "koa-router";
+import HttpError from "../lib/HttpError";
+import logger from "../lib/Logger";
+import config from "../lib/Config";
 import * as querystring from "querystring";
+const axios = require("axios");
+import * as _ from "lodash";
 import * as crypto from "crypto";
 
-const router = new Router({prefix: '/iiif/search'}),
-EM_PATTERN = new RegExp("<em>(.+?)</em>"),
-RESPONSE_TEMPLATE = {
-"@context":[
-"https://iiif.io/api/presentation/3/context.json",
-"https://iiif.io/api/search/1/context.json"
-],
-"@id": '',
-"@type":"sc:AnnotationList",
+const router = new Router({ prefix: "/iiif/search" });
 
-"within": {
-"@type": "sc:Layer",
-"total": 0,
-"ignored": [] as any
-},
-
-"resources": [] as any,
-"hits": [] as any
+const RESPONSE_TEMPLATE_MANIFEST = {
+  "@context": [
+    "https://iiif.io/api/presentation/3/context.json",
+    "https://iiif.io/api/search/1/context.json",
+  ],
+  "@id": "",
+  "@type": "sc:AnnotationList",
+  within: {
+    "@type": "sc:Layer",
+    total: 0,
+    ignored: [] as any,
+  },
+  resources: [] as any,
+  hits: [] as any,
 };
 
-router.get('/manifest/:id', async ctx => {
-    logger.info(`Received a search request for ${ctx.params.id}`);
-
-    ctx.body = JSON.stringify(await search(ctx, ctx.params.id));
-    ctx.set('Content-Type', 'application/json');
-
-    logger.info(`Sending a IIIF collection with id ${ctx.params.id}`);
-});
-
-router.get('/solr/', async ctx => {
-    logger.info(`Received a solr request`);
-
-    const solrBase = `http://${config.solr!.host}:${config.solr!.port}/solr/${config.solr!.core}`,
-        solrUrl = solrBase + "/select?" + ctx.querystring;
-
-    try {
-        logger.info(`${solrUrl}`);
-        const response = await axios.get(solrUrl);
-
-        ctx.body = response.data;
-        ctx.set('Content-Type', 'application/json');
-    } catch (error) {
-        logger.error('Error', error as any);
-        ctx.body = '{error: ' + (error as any) + '}';
-    }
-});
-
-async function search(ctx: Context, docId: string) : Promise<object> {
-    const query = ctx.request.query.q,
-        fq = docId ? `source:${docId}` : '*:*',
-        response = await querySolr(query as string, fq);
-
-    logger.debug('query: ' + query + ', docid: ' + docId);
-
-    let ignoredParams : any[] = [];
-    return makeResponse(response, ignoredParams, docId, query as string);
-}
-
-async function querySolr(query: string, fq: string) : Promise<object> {
-    const params = {
-        'q': query,
-        'df': 'ocr_text',
-        'fq': fq,
-        'rows': 500,
-        'hl': 'on',
-        'hl.ocr.fl': 'ocr_text',
-        'hl.ocr.absoluteHighlights': 'on',
-        'hl.snippets': 4096,
-        'hl.weightMatches': 'true',
-    };
-
-    let out = {
-        'numTotal': 0,
-        'snippets': [] as any
+const RESPONSE_TEMPLATE_COLLECTION = {
+  "@context": "http://iiif.io/api/search/2/context.json",
+  id: "",
+  type: "AnnotationPage",
+  ignored: [] as any,
+  partOf: {
+    id: "",
+    type: "AnnotationCollection",
+    total: 0,
+    first: {
+      id: "",
+      type: "AnnotationPage",
     },
-    solrBase = `http://${config.solr!.host}:${config.solr!.port}/solr/${config.solr!.core}`,
-    solrUrl = solrBase + "/select?" + querystring.stringify(params);
-    logger.debug('solrUrl: ' + solrUrl);
+    last: {
+      id: "",
+      type: "AnnotationPage",
+    },
+  },
+  prev: {
+    id: "",
+    type: "AnnotationPage",
+  } as any,
+  next: {
+    id: "",
+    type: "AnnotationPage",
+  } as any,
+  items: [] as any,
+};
 
-    try {
-        const response = await axios.get(solrUrl),
-            ocrHighlighting = response.data.ocrHighlighting,
-            docs:any[] = response.data.response.docs;
+// Route handlers
+router.get("/manifest/:id", async (ctx) => handleManifestSearch(ctx));
+router.get("/collection/:id", async (ctx) => handleCollectionSearch(ctx));
+router.get("/collection/:id/raw", async (ctx) =>
+  handleCollectionSearch(ctx, true)
+);
 
-        _.forIn(ocrHighlighting, function (document: any, documentId: string) {
+// Helper functions
+async function handleManifestSearch(ctx: Context) {
+  logger.info(
+    `Received a search request for manifest with ID: ${ctx.params.id}`
+  );
+  const id = ctx.params.id;
+  const { q, ...ignored } = normalizeQueryParams(ctx.request.query);
+  const fq = id ? `source:${id}` : "*:*";
 
-            const doc = docs.find(d => d.id === documentId );
-            _.each(document.ocr_text.snippets, function (snippet: any) {
-                snippet.documentId = documentId;
-                snippet.collectionId = doc.source;
-                snippet.imageUrl = doc.image_url;
-                out.snippets.push(snippet);
-            });
-            out.numTotal += document.ocr_text.numTotal;
-        });
+  const solrParams = createSolrParams(q, fq);
+  const response = await querySolr(solrParams);
+  const preprocessedResponse = preprocessManifestResponse(response);
+  const iiifResponse = generateIIIFManifestResponse(
+    preprocessedResponse,
+    Object.keys(ignored),
+    id,
+    q
+  );
 
-    } catch (error) {
-        logger.error('Error', error as any);
-    }
-
-    return out;
+  ctx.body = JSON.stringify(iiifResponse);
+  ctx.set("Content-Type", "application/json");
 }
 
-async function makeResponse(hlresp: any, ignored_fields: any[], volId: string, query: string) : Promise<object> {
-    let doc = _.cloneDeep(RESPONSE_TEMPLATE);
+async function handleCollectionSearch(ctx: Context, raw = false) {
+  logger.info(`Received a collection search request for ${ctx.params.id}`);
+  const id = ctx.params.id;
+  const {
+    q,
+    page = 0,
+    rows = config.solr!.maxRows,
+    ...ignored
+  } = normalizeQueryParams(ctx.request.query);
+  const solrParams = createCollectionSolrParams(
+    id,
+    q,
+    Number(rows),
+    Number(page)
+  );
+  const response = await querySolr(solrParams);
+  const iiifResponse = generateIIIFCollectionResponse(
+    response,
+    Object.keys(ignored),
+    id,
+    q,
+    Number(page),
+    Number(rows)
+  );
 
-    doc['@id'] = config.manifestSearchUrl + '/' + volId + '?q=' + query;
-    doc.within.total = hlresp['numTotal'];
-    doc.within.ignored = ignored_fields;
+  // ctx.body = JSON.stringify(response);
+  ctx.body = JSON.stringify(raw ? response : iiifResponse);
+  ctx.set("Content-Type", "application/json");
+}
 
-    _.each(hlresp.snippets, function (snippet: any, idx: number){
-        const match = snippet.text.match(EM_PATTERN),
-            text = match[1],
-            before = snippet.text.substr(0, match.index),
-            after = snippet.text.substr(match.index + match[0].length);
+function createSolrParams(query: string, fq: string) {
+  return {
+    q: query,
+    df: "ocr_text",
+    fq,
+    rows: 500,
+    hl: "on",
+    "hl.ocr.fl": "ocr_text",
+    "hl.ocr.absoluteHighlights": "on",
+    "hl.snippets": 4096,
+    "hl.weightMatches": "true",
+  };
+}
 
-        _.each(snippet.highlights, function (highlight:any) {
-            let annoIds: string[] = [];
+function createCollectionSolrParams(
+  id: string,
+  q: string,
+  rows: number,
+  page = 0
+) {
+  return {
+    q: `(${q}) AND source:${id}*`,
+    df: "ocr_text",
+    hl: "true",
+    "hl.ocr.fl": "ocr_text",
+    "hl.ocr.contextBlock": "line",
+    "hl.ocr.contextSize": "1",
+    "hl.snippets": "1",
+    group: "true",
+    "group.field": "source",
+    "group.sort": "id asc",
+    "group.limit": "1",
+    "group.format": "simple",
+    "group.ngroups": "true",
+    rows,
+    start: page * rows,
+  };
+}
 
-            _.each(highlight, function (highlightBox:any) {
-                const ident = config.manifestSearchUrl  + '/' + snippet.documentId + '/annotation/' +crypto.randomBytes(16).toString("hex"),
-                    x = highlightBox.ulx,
-                    y = highlightBox.uly,
-                    w = highlightBox.lrx - highlightBox.ulx,
-                    h = highlightBox.lry - highlightBox.uly;
+async function querySolr(params: any): Promise<any> {
+  const solrUrl = buildSolrUrl(params);
+  logger.debug("solrUrl: " + solrUrl);
 
-                annoIds.push(ident);
+  try {
+    const response = await axios.get(solrUrl);
+    return response.data;
+  } catch (error) {
+    logger.error("Error in Solr query: ", error);
+    throw new HttpError(500, "Error querying Solr");
+  }
+}
 
-                doc.resources.push({
-                    "@id": ident,
-                    "@type": "oa:Annotation",
-                    "motivation": "sc:painting",
-                    "resource": {
-                        "@type": "cnt:ContentAsText",
-                        "chars": highlightBox.text
-                    },
-                    "on": config.imageServerUrl + '/iiif/2/' + snippet.imageUrl + '#xywh=' + x + ',' + y + ',' + w + ','+ h
-                });
-            });
+function buildSolrUrl(params: any) {
+  const solrBase = `http://${config.solr!.host}:${config.solr!.port}/solr/${
+    config.solr!.core
+  }`;
+  return `${solrBase}/select?${querystring.stringify(params)}`;
+}
 
-            doc.hits.push({
-                '@type': 'search:Hit',
-                'annotations': annoIds,
-                'match': text,
-                'before': before,
-                'after': after,
-            });
-        });
+function preprocessManifestResponse(response: any) {
+  let out = {
+    numTotal: 0,
+    snippets: [] as any,
+  };
+  try {
+    const ocrHighlighting = response.ocrHighlighting,
+      docs: any[] = response.response.docs;
+
+    _.forIn(ocrHighlighting, function (document: any, documentId: string) {
+      const doc = docs.find((d) => d.id === documentId);
+      _.each(document.ocr_text.snippets, function (snippet: any) {
+        snippet.documentId = documentId;
+        snippet.collectionId = doc.source;
+        snippet.imageUrl = doc.image_url;
+        out.snippets.push(snippet);
+      });
+      out.numTotal += document.ocr_text.numTotal;
     });
+  } catch (error) {
+    logger.error("Error", error as any);
+  }
+  return out;
+}
 
-    return doc;
+function generateIIIFManifestResponse(
+  solrResponse: any,
+  ignoredFields: any[],
+  volId: string,
+  query: string
+) {
+  let doc = _.cloneDeep(RESPONSE_TEMPLATE_MANIFEST);
+
+  doc["@id"] = config.manifestSearchUrl + "/" + volId + "?q=" + query;
+  doc.within.total = solrResponse["numTotal"];
+  doc.within.ignored = ignoredFields;
+
+  _.each(solrResponse.snippets, function (snippet: any, idx: number) {
+    const match = snippet.text.match(/<em>(.+?)<\/em>/),
+      text = match[1],
+      before = snippet.text.substr(0, match.index),
+      after = snippet.text.substr(match.index + match[0].length);
+
+    _.each(snippet.highlights, function (highlight: any) {
+      let annoIds: string[] = [];
+
+      _.each(highlight, function (highlightBox: any) {
+        const ident =
+            config.manifestServerUrl +
+            "/" +
+            snippet.documentId +
+            "/annotation/" +
+            crypto.randomBytes(16).toString("hex"),
+          x = highlightBox.ulx,
+          y = highlightBox.uly,
+          w = highlightBox.lrx - highlightBox.ulx,
+          h = highlightBox.lry - highlightBox.uly;
+
+        annoIds.push(ident);
+
+        doc.resources.push({
+          "@id": ident,
+          "@type": "oa:Annotation",
+          motivation: "sc:painting",
+          resource: {
+            "@type": "cnt:ContentAsText",
+            chars: highlightBox.text,
+          },
+          on:
+            config.imageServerUrl +
+            "/iiif/2/" +
+            snippet.imageUrl +
+            "#xywh=" +
+            x +
+            "," +
+            y +
+            "," +
+            w +
+            "," +
+            h,
+        });
+      });
+
+      doc.hits.push({
+        "@type": "search:Hit",
+        annotations: annoIds,
+        match: text,
+        before: before,
+        after: after,
+      });
+    });
+  });
+
+  return doc;
+}
+
+function generateIIIFCollectionResponse(
+  solrResponse: any,
+  ignoredFields: any[],
+  volId: string,
+  query: string,
+  page: number,
+  rows: number
+) {
+  let doc = _.cloneDeep(RESPONSE_TEMPLATE_COLLECTION);
+  let items = [] as any;
+  const total = solrResponse.grouped.source.ngroups;
+  const last = Math.floor(Math.abs((total - 1) / rows));
+
+  doc.id = `${config.collectionSearchUrl}/${volId}?q=${query}${
+    page > 0 ? `&page=${page}` : ""
+  }`;
+  doc.ignored = ignoredFields;
+  doc.partOf.id = `${config.collectionSearchUrl}/${volId}?q=${query}`;
+  doc.partOf.total = total;
+  doc.partOf.first.id = `${config.collectionSearchUrl}/${volId}?q=${query}`;
+  doc.partOf.last.id = `${config.collectionSearchUrl}/${volId}?q=${query}&page=${last}`;
+
+  if (page > 0) {
+    doc.prev.id = `${config.collectionSearchUrl}/${volId}?q=${query}&page=${
+      page - 1
+    }`;
+  } else {
+    delete doc.prev;
+  }
+  if (page < last) {
+    doc.next.id = `${config.collectionSearchUrl}/${volId}?q=${query}&page=${
+      page + 1
+    }`;
+  } else {
+    delete doc.next;
+  }
+
+  _.each(solrResponse.grouped.source.doclist.docs, function (doc: any) {
+    const randomBytes = crypto.randomBytes(16).toString("hex");
+    const snippet = solrResponse.ocrHighlighting[doc.id].ocr_text.snippets[0];
+    const x = snippet.highlights[0][0].ulx;
+    const y = snippet.highlights[0][0].uly;
+    const w = snippet.highlights[0][0].lrx - snippet.highlights[0][0].ulx;
+    const h = snippet.highlights[0][0].lry - snippet.highlights[0][0].uly;
+
+    const item = {
+      id: `${config.imageServerUrl}/iiif/2/${doc.id}/annotation/${randomBytes}`,
+      type: "Annotation",
+      motivation: "highlighting",
+      body: {
+        type: "TextualBody",
+        value: snippet.text,
+        format: "text/html",
+      },
+      target: {
+        id: `${config.imageServerUrl}/iiif/2/${doc.image_url}#xywh=${x},${y},${w},${h}`,
+        partOf: {
+          id: `${doc.manifest_path || ""}`,
+          type: "Manifest",
+          label: {
+            en: [doc.manifest_label || ""],
+          },
+        },
+      },
+    };
+    items.push(item);
+  });
+  doc.items = items;
+
+  return doc;
+}
+
+function normalizeQueryParams(query: querystring.ParsedUrlQuery): {
+  [key: string]: string;
+} {
+  const normalizedParams: { [key: string]: string } = {};
+
+  for (const key in query) {
+    const value = query[key];
+    if (Array.isArray(value)) {
+      normalizedParams[key] = value[value.length - 1];
+    } else if (typeof value === "string") {
+      normalizedParams[key] = value;
+    } else {
+      throw new Error("Unexpected value type");
+    }
+  }
+
+  return normalizedParams;
 }
 
 export default router;
